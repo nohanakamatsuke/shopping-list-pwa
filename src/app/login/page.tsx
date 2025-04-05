06da54555fecbccc0e6a020bf5e46a08166c88db
nohanakamatsuke <nohana115ron@icloud.com>
2025-04-05 13:17:38 +0900

    ログイン認証機能作成

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,18 +1,50 @@
+'use client';
+
 import Link from 'next/link';
-import React from 'react';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
+import { auth } from '../lib/firebase';
+import { signInWithEmailAndPassword } from 'firebase/auth/web-extension';
 
 const LoginPage: React.FC = () => {
-	return (
-		<div className='min-h-screen flex items-center justify-center'>
-			<div className='w-full max-w-md px-6'>
-			  <form className='space-y-5'>
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent) =>{
+    e.preventDefault();
+    try{
+      // メールアドレスとパスワードでサインイン
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      alert('ログインしました！');
+      // ホーム画面に遷移
+      router.push('/');
+    }
+    catch(error){
+      console.error(error);
+      // エラーコードに基づいてメッセージをアラートで表示
+    }
+  };
+
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+  return (
+			<div className='w-full max-w-lg px-6'>
+			  <form 
+          onSubmit={handleSubmit}
+          className='space-y-5'>
 					<div className=''>
 						<label htmlFor="email" className='block text-lg'>メールアドレス</label>
-						<input type="email" id="email" name="email" required className="w-full p-3 rounded text-black bg-white focus:outline-none"/>
+						<input type="email" id="email" name="email" 
+            required 
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-full p-3 rounded text-black bg-white focus:outline-none"/>
 					</div>
 					<div>
 						<label htmlFor="password" className='block text-lg'>パスワード</label>
-						<input type="password" id="password" name="password" required className="w-full p-3 rounded text-black bg-white focus:outline-none text-black"/>
+						<input type="password" id="password" name="password" 
+            required 
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full p-3 rounded text-black bg-white focus:outline-none text-black"/>
 					</div>
 					<button type="submit" className='w-full mt-5 px-2 py-3 bg-[#FF5154] rounded-full text-lg font-bold'>ログイン</button>
 					<div className="mt-6 text-center">
@@ -22,8 +54,6 @@ const LoginPage: React.FC = () => {
           </div>
 				 </form>
 			</div>
-				
-		</div>
 	);
 };
 export default LoginPage;
